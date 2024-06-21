92ab829452501b1f36e2504f9afab3210507b5d4
Nelson-Aguilar <157563219+Nelson-Aguilar@users.noreply.github.com>
2024-06-21 15:07:08 -0600

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -40,7 +40,7 @@ const Hero = () => {
     // Establecer un temporizador para cambiar el video cada 2 segundos
     const intervalId = setInterval(() => {
       actualizarVideoAleatorio();
-    }, 3000);
+    }, 2000);
 
     // Limpiar el temporizador al desmontar el componente
     return () => clearInterval(intervalId);
